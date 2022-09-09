1350a16689e88e7bbcbe243263bfde1a5c742050
Jivko <jivkon1410@gmail.com>
2022-09-09 23:04:28 +0300

    added makefile

--- network.py
+++ network.py
@@ -2,7 +2,7 @@ import socket
 
 class Network:
     def __init__(self):
-        self.client = socket.socket(socket.IF_INET, socket.SOCK_STREAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "192.168.0.27"
         self.port = 5555
         self.addr = (self.server, self.port)
