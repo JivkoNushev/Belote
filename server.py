7437418497e0f52edef7097d1edd124ff2e2148e
Jivko <jivkon1410@gmail.com>
2022-09-09 16:58:03 +0300

    added threaded clients

--- server.py
+++ server.py
@@ -1,3 +1,34 @@
 import socket
-import _thread
-import os
+from _thread import *
+import sys
+
+server = ""
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    print(str(e))
+
+s.listen(4)
+print("Waiting for connection")
+
+def threaded_client(conn):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+        
