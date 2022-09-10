938e3169754f41a2719941f1ec1876b25864d12a
Jivko <jivkon1410@gmail.com>
2022-09-10 21:20:39 +0300

    updated sending data

--- client.py
+++ client.py
@@ -19,6 +19,18 @@ class Player():
         self.rect = (x, y, width, height)
         self.velocity = 3
 
+    def get_color(self):
+        return self.color
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_pos(self):
+        return (self.x, self.y)
+
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
 
@@ -37,17 +49,115 @@ class Player():
         if keys[pygame.K_LEFT]:
             self.x -= self.velocity
 
+        po = pygame.mouse.get_pressed()
+        if po[0]:
+            mPos = pygame.mouse.get_pos()
+            print(mPos)
+            if (self.x <= mPos[0] and mPos[0] <= self.x + self.width) and (self.y <= mPos[1] and mPos[1] <= self.y + self.height):
+                self.color = (0,0,255)
+            else:
+                self.color = (0,255,0)
+
+
         self.update()
 
     def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
 
 def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+    item = ""
+    items = []
+    for s in str:
+        if s == "(" or s == "," or s == ")":
+            if item != "":
+                items.append(int(item))
+                item = ""
+            continue
+        item += s
+
+    res = []
+    while items:
+        res.append((items[0], items[1]))
+        items.pop(0)
+        items.pop(0)
+
+    return res
 
 def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
+    res = ""
+    count = 0
+    for item in tup:
+        if type(item) == int:
+            if count % 2 == 0:
+                res += "("
+                res += str(item) + ","
+            else:
+                res += str(item) + "),"
+        else:
+            res += "("
+            res += str(item[0]) + ","
+            res += str(item[1]) + "),"
+        count += 1
+    return res[0:-1]
+
+def player_data_todict(players):
+    d = dict()
+    count = 0
+    for player in players:
+        p = dict()
+        p["color"] = player.get_color()
+        p["pos"] = player.get_pos()
+        p["width"] = player.get_width()
+        p["height"] = player.get_height()
+        d[count] = p
+        count += 1 
+    return d
+
+def player_dict_tostr(playerDict):
+    return str(playerDict)
+
+def player_str_todict(playerDictString):
+    res = dict()
+    playerDictString = playerDictString[1:-1]
+    count = 0
+    while playerDictString:
+        res2 = dict()
+        if playerDictString[0] == "}":
+            break
+        while playerDictString[0] != "{":
+            playerDictString = playerDictString[1:]
+        playerDictString = playerDictString[1:]
+        
+        for i in range(4):
+            key = ""
+            while playerDictString[0] != "'":
+                playerDictString = playerDictString[1:]
+            playerDictString = playerDictString[1:]
+            
+            while playerDictString[0] != "'":
+                key += playerDictString[0]
+                playerDictString = playerDictString[1:]
+            playerDictString = playerDictString[1:]
+            
+            while(playerDictString[0] == ":" or playerDictString[0] == " " or playerDictString[0] == "," or playerDictString[0] == "}"):
+                playerDictString = playerDictString[1:]
+                
+            item = ""
+            if playerDictString[0] == "(":
+                playerDictString = playerDictString[1:]
+                while playerDictString[0] != ")":
+                    item += playerDictString[0]
+                    playerDictString = playerDictString[1:]
+                playerDictString = playerDictString[1:]
+            else:
+                while playerDictString[0] != "," and playerDictString[0] != "}":
+                    item += playerDictString[0]
+                    playerDictString = playerDictString[1:]
+            playerDictString = playerDictString[1:]
+            res2[key] = eval(item)
+        res[count] = res2.copy()
+        count += 1
+    return res
 
 def redrawWindow(win, players):
     win.fill((255,255,255))
@@ -60,21 +170,25 @@ def main():
     run = True
     n = Network()
     startPos = read_pos(n.getPos())
-    players = [Player(startPos[0], startPos[1],100,100,(0,255,0)), Player(0,0,100,100,(0,255,0)), Player(0,0,100,100,(0,255,0)), Player(0,0,100,100,(0,255,0))]
+    players = [Player(startPos[0][0], startPos[0][1],100,100,(0,255,0)), Player(0,0,100,100,(255,0,0)), Player(0,0,100,100,(255,0,0)), Player(0,0,100,100,(255,0,0))]
+    
     p = players[0]
 
     clock = pygame.time.Clock()
 
+    playersDict = player_data_todict(players)
+    #print(player_dict_tostr(playersDict))
     while run:
         clock.tick(60)
-        pPos = read_pos(n.send(make_pos((p.x, p.y))))
-        count = 0
 
+        pDict = player_str_todict(n.send(player_dict_tostr(playersDict)))
+        print(pDict)
+        count = 0
         for player in players:
-            if count > 0:
-                player.x = pPos[count][0]
-                player.y = pPos[count][1]
-                player.update()
+            #print(pDict)
+            player.x = pDict[count]["pos"][0]
+            player.y = pDict[count]["pos"][1]
+            player.update()
             count += 1
 
         for event in pygame.event.get():
