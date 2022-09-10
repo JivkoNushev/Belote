23bc564bb106a3dbda2d3d9bb2d6a13b9064093e
Jivko <jivkon1410@gmail.com>
2022-09-10 21:20:39 +0300

    updated sending data

--- server.py
+++ server.py
@@ -16,18 +16,99 @@ s.listen(4)
 print("Waiting for connection")
 
 def read_pos(str):
-    print(str)
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+    item = ""
+    items = []
+    for s in str:
+        if s == "(" or s == "," or s == ")":
+            if item != "":
+                items.append(int(item))
+                item = ""
+            continue
+        item += s
+
+    res = []
+    while items:
+        res.append((items[0], items[1]))
+        items.pop(0)
+        items.pop(0)
+
+    return res
 
 def make_pos(tup):
     res = ""
+    count = 0
     for item in tup:
-        print(item)
-        res += str(item[0]) + ","
-        res += str(item[1]) + ","
-    print(res[0:-1])
+        if type(item) == int:
+            if count % 2 == 0:
+                res += "("
+                res += str(item) + ","
+            else:
+                res += str(item) + "),"
+        else:
+            res += "("
+            res += str(item[0]) + ","
+            res += str(item[1]) + "),"
+        count += 1
     return res[0:-1]
+
+def player_data_todict(players):
+    d = dict()
+    count = 0
+    for player in players:
+        p = dict()
+        p["color"] = player.get_color()
+        p["pos"] = player.get_pos()
+        p["width"] = player.get_width()
+        p["height"] = player.get_height()
+        d[count] = p
+        count += 1 
+    return d
+
+def player_dict_tostr(playerDict):
+    return str(playerDict)
+
+def player_str_todict(playerDictString):
+    res = dict()
+    playerDictString = playerDictString[1:-1]
+    count = 0
+    while playerDictString:
+        res2 = dict()
+        if playerDictString[0] == "}":
+            break
+        while playerDictString[0] != "{":
+            playerDictString = playerDictString[1:]
+        playerDictString = playerDictString[1:]
+        
+        for i in range(4):
+            key = ""
+            while playerDictString[0] != "'":
+                playerDictString = playerDictString[1:]
+            playerDictString = playerDictString[1:]
+            
+            while playerDictString[0] != "'":
+                key += playerDictString[0]
+                playerDictString = playerDictString[1:]
+            playerDictString = playerDictString[1:]
+            
+            while(playerDictString[0] == ":" or playerDictString[0] == " " or playerDictString[0] == "," or playerDictString[0] == "}"):
+                playerDictString = playerDictString[1:]
+                
+            item = ""
+            if playerDictString[0] == "(":
+                playerDictString = playerDictString[1:]
+                while playerDictString[0] != ")":
+                    item += playerDictString[0]
+                    playerDictString = playerDictString[1:]
+                playerDictString = playerDictString[1:]
+            else:
+                while playerDictString[0] != "," and playerDictString[0] != "}":
+                    item += playerDictString[0]
+                    playerDictString = playerDictString[1:]
+            playerDictString = playerDictString[1:]
+            res2[key] = eval(item)
+        res[count] = res2.copy()
+        count += 1
+    return res
 
 pos = [(0, 0), (100, 100), (200, 200), (300, 300)]
 
@@ -36,27 +117,26 @@ def threaded_client(conn, player):
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode("utf-8"))
-            pos[player] = data
+            PlayerData = player_str_todict(conn.recv(2048).decode("utf-8"))
             
-            if not data:
+            pos[player] = PlayerData[player]["pos"]
+            if not PlayerData:
                 print("Disconnected")
                 break
-            else:
-                #print(player)
-                if player == 0:
-                    reply = (pos[1], pos[2], pos[3])
-                elif player == 1:
-                    reply = (pos[0], pos[2], pos[3])
-                elif player == 2:
-                    reply = (pos[0], pos[1], pos[3])
-                else:
-                    reply = (pos[0], pos[1], pos[2])
+            # else:
+            #     if player == 0:
+            #         reply = (pos[1], pos[2], pos[3])
+            #     elif player == 1:
+            #         reply = (pos[0], pos[2], pos[3])
+            #     elif player == 2:
+            #         reply = (pos[0], pos[1], pos[3])
+            #     else:
+            #         reply = (pos[0], pos[1], pos[2])
                
-                print("Received: ", data)
-                print("Sending: ", reply)
+            #    print("Received: ", data[0])
+            #    print("Sending: ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(str.encode(player_dict_tostr(PlayerData)))
         except:
             break
     
