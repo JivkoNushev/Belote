115e22242eca49a697f5786f7ec3d79ebcbd7c4b
Jivko <jivkon1410@gmail.com>
2022-09-26 20:34:11 +0300

    add

--- player.py
+++ player.py
@@ -3,7 +3,6 @@ from turtle import heading, width
 import pygame
 
 class Player():
-<<<<<<< HEAD
     def __init__(self, id):
         self.id = id
         self.cards = []
@@ -11,20 +10,6 @@ class Player():
     def get_id(self):
         return self.id
     
-=======
-    def __init__(self, id, cards):
-        self.id = id
-        if type(cards) != list:
-            self.cards = [cards]
-        elif cards == 0:
-            cards = []
-        else:
-            self.cards = cards
-
-    def get_id(self):
-        return self.id
-
->>>>>>> 63aadb6c9ceb074bbf7cf3a172d51458f6fc1493
     def get_cards(self):
         return self.cards
 
@@ -38,11 +23,8 @@ class Player():
     def draw(self, win):
         for card in self.cards:
             card.draw(win)
-<<<<<<< HEAD
 
     def deal(self, cards):
         for card in cards:
             self.cards.append(card)
 
-=======
->>>>>>> 63aadb6c9ceb074bbf7cf3a172d51458f6fc1493
